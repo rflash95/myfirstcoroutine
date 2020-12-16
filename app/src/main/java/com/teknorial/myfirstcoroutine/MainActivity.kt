4db18b6a2102d181626f388c1b3800ab762435c9
Reza Maulana <rflash95@gmail.com>
2020-12-17 01:02:47 +0700

    suspend function

--- app/src/main/java/com/teknorial/myfirstcoroutine/MainActivity.kt
+++ app/src/main/java/com/teknorial/myfirstcoroutine/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.teknorial.myfirstcoroutine
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -14,14 +14,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //delay = suspend function
+        //doNetworkCall() suspend functiom hanya dapat dipanggil di dalam coroutine atau suspend function lainnya
+
+
         //GlobalScope :  Coroutine akan tetap hidup selama aplikasi kita berjalan, jika tidak selesai sebelumnya
         GlobalScope.launch {
             //delay : hanya membuat coroutine tertidur, tidak keseluruhan thread
-            delay(3000)
+            delay(3000L)
             Log.d(TAG, "Coroutine says hello from thread ${Thread.currentThread().name}")
+
+
+            val networkCallAnswer = doNetworkCall()
+            val networkCallAnswer2 = doNetworkCall2()
+
+            Log.d(TAG, networkCallAnswer)
+            Log.d(TAG, networkCallAnswer2)
         }
 
         Log.d(TAG, "Hello from thread ${Thread.currentThread().name}")
 
+    }
+
+    //membuat suspend function
+    suspend fun doNetworkCall(): String {
+        delay(4000L)
+        return "This is the answer"
+    }
+
+    //membuat suspend function
+    suspend fun doNetworkCall2(): String {
+        delay(4000L)
+        return "this is the answer"
     }
 }
